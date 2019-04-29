84a275c23faa241d6915590d25ed05455d19d406
sycrus <jzhchung@gmail.com>
2019-04-28 21:59:59 -0400

    initial commit

---
+++ anim test/Assets/Scripts/DanceController.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceController : MonoBehaviour
+{
+    Animator animator;
+    bool isDance1 = false;
+    bool isDance2 = false;
+    bool isDance3 = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    /*
+
+    void OnColliionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            Destroy(collision.gameObject);
+            if (!isDance1)
+            {
+                animator.SetBool("dance1", true);
+                isDance1 = !isDance1;
+                Debug.Log("isDance1 " + isDance1);
+            }
+            else
+            {
+                animator.SetBool("dance1", false);
+                isDance1 = !isDance1;
+                Debug.Log("isDance1 " + isDance1);
+            }
+        }
+    }
+    */
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("Q key was pressed.");
+
+            if (!isDance1)
+            {
+                animator.SetBool("dance1", true);
+                isDance1 = !isDance1;
+                Debug.Log("isDance1 " + isDance1);
+            }
+            else
+            {
+                animator.SetBool("dance1", false);
+                isDance1 = !isDance1;
+                Debug.Log("isDance1 " + isDance1);
+            }
+
+
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("W was pressed.");
+
+
+            if (!isDance2)
+            {
+                animator.SetBool("dance2", true);
+                isDance2 = !isDance2;
+                Debug.Log("isDance2 " + isDance2);
+            }
+            else
+            {
+                animator.SetBool("dance2", false);
+                isDance2 = !isDance2;
+                Debug.Log("isDance2 " + isDance2);
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E key was pressed.");
+
+
+            if (!isDance3)
+            {
+                animator.SetBool("dance3", true);
+                isDance3 = !isDance3;
+                Debug.Log("isDance3 " + isDance3);
+            }
+            else
+            {
+                animator.SetBool("dance3", false);
+                isDance3 = !isDance3;
+                Debug.Log("isDance3 " + isDance3);
+            }
+
+        }
+    }
+}
---
+++ anim test/Assets/Scripts/InstantiateCubes.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateCubes : MonoBehaviour
+{
+    public GameObject _sampleCubePrefab;
+    GameObject[] _sampleCube = new GameObject[512];
+    public float _maxScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+        for (int i = 0; i < 512; i++)
+        {
+            GameObject _instantSampleCube = (GameObject)Instantiate(_sampleCubePrefab);
+            _instantSampleCube.transform.position = this.transform.position;
+            _instantSampleCube.transform.parent = this.transform;
+            _instantSampleCube.name = "SampleCube" + i;
+            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            _instantSampleCube.transform.position = Vector3.forward * 100;
+            _sampleCube[i] = _instantSampleCube;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            if (_sampleCube !=null)
+            {
+                //Debug.Log(FindObjectOfType<AudioSpec>()._samples[i]);
+                _sampleCube[i].transform.localScale = new Vector3 (10, (FindObjectOfType<AudioSpec>()._samples [i] * _maxScale) + 2, 10);
+            }
+        }
+    }
+}
